0b1057d39e115881e8e0fb3a2414a36a36bd9efc
Li Yazhou <liyazhou@fenbi.com>
2020-11-21 00:16:56 +0800

    add scaffold about kube ssh

--- go.mod
+++ go.mod
@@ -5,5 +5,12 @@ go 1.14
 require (
 	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/gliderlabs/ssh v0.3.1
+	github.com/googleapis/gnostic v0.5.3 // indirect
 	golang.org/x/crypto v0.0.0-20201117144127-c1f2f97bffc9 // indirect
+	golang.org/x/oauth2 v0.0.0-20201109201403-9fd604954f58 // indirect
+	golang.org/x/time v0.0.0-20200630173020-3af7569d3a1e // indirect
+	k8s.io/api v0.19.4
+	k8s.io/client-go v11.0.0+incompatible
+	k8s.io/klog v1.0.0 // indirect
+	k8s.io/utils v0.0.0-20201110183641-67b214c5f920 // indirect
 )
--- main.go
+++ main.go
@@ -4,8 +4,78 @@ import (
 	"io"
 	"log"
 
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/deprecated/scheme"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/remotecommand"
+
 	"github.com/gliderlabs/ssh"
 )
+
+type ptyTerminal struct {
+	session              ssh.Session
+	kubeClient           *kubernetes.Clientset
+	kubeRestClientConfig *restclient.Config
+
+	user          string
+	namespace     string
+	podName       string
+	containerName string
+}
+
+func newPtyTerminal(session ssh.Session) *ptyTerminal {
+	return &ptyTerminal{
+		session: session,
+		user:    session.User(),
+	}
+}
+
+func (pty *ptyTerminal) execKubePty(
+	namespace string,
+	podName string,
+	containerName string,
+	command string,
+) error {
+	exec, err := pty.kubeRemoteExecutor()
+	if err != nil {
+		return err
+	}
+
+	err = exec.Stream(remotecommand.StreamOptions{
+		Stdin:  pty.Session,
+		Stdout: pty.Session,
+		Stderr: pty.Session,
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (pty *ptyTerminal) kubeRemoteExecutor() (remotecommand.Executor, error) {
+	option := &v1.PodExecOptions{
+		Container: containerName,
+		Command:   []string{command},
+		Stdin:     true,
+		Stdout:    true,
+		Stderr:    true,
+		TTY:       true,
+	}
+
+	req := pty.kubeClient.
+		CoreV1().
+		RESTClient().
+		Post().
+		Resource("pods").
+		Name(podName).
+		Namespace(namespace).
+		SubResource("exec").
+		Param("container", container).
+		VersionedParams(option, scheme.ParameterCodec)
+
+	return remotecommand.NewSPDYExecutor(pty.kubeRestClientConfig, "POST", req.URL())
+}
 
 func main() {
 	ssh.Handle(func(s ssh.Session) {
